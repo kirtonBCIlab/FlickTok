ae0791d6ed83c3ceea707251e299518520e616fb
arazzz <minhas.araz@gmail.com>
2024-06-17 10:01:37 -0600

    init

--- src/apps/client/src/main.js
+++ src/apps/client/src/main.js
@@ -6,13 +6,15 @@
 import fs from "fs";
 import axios from "axios";
 import waitOn from "wait-on";
-import { app, BrowserWindow, ipcMain } from "electron";
+import { app as electronApp, BrowserWindow, ipcMain } from "electron";
 import { fileURLToPath } from "url";
 import { join, dirname } from "path";
 import { proxy } from "valtio";
 import { subscribeKey } from "valtio/utils";
 import { io as sioClient } from "socket.io-client";
 import { parse } from "yaml";
+import Express from "express";
+import handlebars from "express-handlebars";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -20,137 +22,67 @@ const monoConfig = parse(
   fs.readFileSync(join(__dirname, "../../../packages/config/base.yml"), "utf8")
 );
 const serverURL = `http://${monoConfig.server.host}:${monoConfig.server.port}`; // http://localhost:8000
-const waitOnEndPoint =
+const clientURL = `http://${monoConfig.client.host}:${monoConfig.client.port}`; // http://localhost:8001
+const waitOnPythonEndPoint =
   serverURL.replace("http://", "http-get://") + "/api/healthcheck";
 axios.defaults.baseURL = serverURL;
 
 const sio = sioClient(serverURL, { transports: ["websocket"] });
+const expressApp = Express();
+expressApp.use(Express.json());
+expressApp.use(Express.urlencoded({ extended: false }));
+expressApp.use(Express.static(join(__dirname, "../public")));
+expressApp.engine(
+  "handlebars",
+  handlebars.engine({
+    defaultLayout: "main",
+    layoutsDir: join(__dirname, "../views/layouts"),
+    partialsDir: join(__dirname, "../views/partials"),
+  })
+);
+expressApp.set("view engine", "handlebars");
+expressApp.set("views", join(__dirname, "../views"));
+expressApp.get("/", (req, res) => res.render("index"));
+expressApp.listen(monoConfig.client.port, monoConfig.client.host);
 
-/**
- * app state
- * */
 let win; // main window
 const ctx = proxy({
   serverLoaded: false,
-  navigatedToReels: false,
-  currentReel: {
-    id: null,
-    duration: null,
-    currentTime: null,
-  },
-}); // can subscribe to changes in ctx
+}); // can subscribe to changes in ctx (the application state)
 
-/**
- * electron main window handling
- * & ipc (inter-process communication) setup
- * */
-// create window
-app.on("ready", () => {
+electronApp.on("ready", () => {
+  // create main window
   win = new BrowserWindow({
     width: 800,
     height: 600,
     show: false,
-    webPreferences: {
-      preload: join(__dirname, "preload.mjs"),
-      sandbox: false,
-      contextIsolation: true,
-    },
+    // webPreferences: {
+    //   preload: join(__dirname, "preload.mjs"),
+    //   sandbox: false,
+    //   contextIsolation: true,
+    // },
   });
 
-  // wait for server to be ready
-  waitOn({ resources: [waitOnEndPoint] })
+  // wait for server to be ready before loading the client
+  waitOn({ resources: [waitOnPythonEndPoint] })
     .then(() => (ctx.serverLoaded = true))
     .catch((err) => console.error(" Error while waiting on resources...", err));
-
-  subscribeKey(ctx, "serverLoaded", (v) => {
-    if (!v) return;
-
-    // win.webContents.openDevTools({ mode: "detach" }); // uncomment to open devtools in separate window on start
-
-    if (monoConfig.client.skipLogin === true) {
-      win.loadURL("https://www.instagram.com/reels/");
-      win.webContents.once("did-finish-load", () =>
-        win.webContents.send("login-success")
-      ); // handled in preload.js
-    } else {
-      // login; will redirect to main page after login or if already logged in
-      win.loadURL("https://www.instagram.com/accounts/login/");
-
-      win.webContents.once("did-navigate", (_event, url) => {
-        console.info(
-          `Navigated to: ${url}; navigatedToReels: ${ctx.navigatedToReels}...`
-        );
-        if (url === "https://www.instagram.com/" && !ctx.navigatedToReels) {
-          // on main page (after login) --> navigate to reels
-          console.info(`Navigated to: ${url}; Login successful...`);
-          console.info("Navigating to reels...");
-          win.webContents.on("did-finish-load", () =>
-            win.webContents.send("login-success")
-          ); // handled in preload.js
-        }
-      });
-    }
-
-    win.once("ready-to-show", () => win.show());
-    win.on("closed", () => (win = null));
-  });
-});
-app.on("window-all-closed", () =>
-  process.platform !== "darwin" ? app.quit() : null
-);
-app.on("activate", () => (win === null ? createWindow() : null));
-
-/**
- * electron ipc ([e.g., communication between preload.js & main.js])
- * & socket (client-server communication) handling
- * */
-// listen for 'reels loaded' event
-ipcMain.once("reels-loaded", () => {
-  // win.loadURL("https://www.instagram.com/reels/"); // could have navigated to reels here as well
-  console.info("Reels loaded...");
-  ctx.navigatedToReels = true; // update state; triggers the subscription below
 });
 
-// listen for ctx.navigatedToReels changes
-subscribeKey(ctx, "navigatedToReels", (v) => {
+// server is ready, load the client
+subscribeKey(ctx, "serverLoaded", (v) => {
   if (!v) return;
-  win.webContents.on("did-navigate-in-page", (event, url) => {
-    const reelId = /instagram\.com\/reels\/.+\//.test(url)
-      ? url.split("/").at(-2)
-      : null; // extract reel id from url
 
-    if (reelId && reelId !== ctx.currentReel.id) {
-      ctx.currentReel.id = reelId;
+  // win.webContents.openDevTools({ mode: "detach" }); // uncomment to open devtools in separate window on start
 
-      console.info(`Current reel id: ${reelId}`);
+  win.loadURL(clientURL); // client loads at http://localhost:8001 by default
 
-      // region - request reel info (e.g., duration) from preload.js
-      const getCurrentReelInfoRequest = () =>
-        win.webContents.send("get-current-reel-info");
-      win.webContents.once("did-finish-load", getCurrentReelInfoRequest); // needed for the first time
-      getCurrentReelInfoRequest(); // subsequent requests
-      // endregion
-
-      ipcMain.once("set-current-reel-info", (event, data) => {
-        // update state
-        ctx.currentReel = { ...ctx.currentReel, ...data };
-
-        // Let the server know that we are watching this reel; handled in server/src/app.py
-        sio.emit("loaded_new_reel", data);
-
-        win.webContents.removeListener(
-          "did-finish-load",
-          getCurrentReelInfoRequest
-        ); // avoid memory leak
-      });
-    }
-  });
+  win.once("ready-to-show", () => win.show());
+  win.on("closed", () => (win = null));
 });
 
-// listen for 'switch_to_new_reel_at_time' message from server (server/src/app.py)
-sio.on("switch_to_new_reel_at_time", ({ reelId, time }) => {
-  console.info(
-    `Python told me to swipe away from reel ${reelId} & get a new one at time: ${time}...`
-  );
-  win.webContents.send("switch-to-new-reel-at-time", { reelId, time });
-});
+// electron - general window events
+electronApp.on("window-all-closed", () =>
+  process.platform !== "darwin" ? electronApp.quit() : null
+);
+electronApp.on("activate", () => (win === null ? createWindow() : null));
